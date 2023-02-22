54b5510d0b3dc6b8c727befa8a604760c619c002
giuliatognali <giuliatognali@gmail.com>
2023-02-22 17:30:55 +0100

    const input

--- js/main.js
+++ js/main.js
@@ -3,19 +3,35 @@
 const inputDistance = document.getElementById('input-distance');
 const ageRange = document.getElementById('age-range');
 
-//let ticketPrice = inputDistance * 0.21;
+
+let ticketPrice = inputDistance * 0.21;
+
+let messaggio = 'Prezzo biglietto';
+
+const ageOver18 = document.getElementById('over18');
+const ageUnder18 = document.getElementById('under18');
+const ageOver65 = document.getElementById('over65');
 
 const submitBtn = document.getElementById('submit-btn');
 
+if (ageRange === ageUnder18) {
+    ticketPrice = ticketPrice - ticketPrice * 0.2;
+    messaggio = 'Prezzo con sconto minorenni'
+//    ticketPrice -= ticketPrice * 0,2; altro modo per scrivere quest'operazione
+} 
+else if (ageRange === ageOver65) {
+    ticketPrice = ticketPrice - ticketPrice * 0.4;
+    messaggio = 'Prezzo con sconto over 65'
+}
 
 
 submitBtn.addEventListener('click', 
     function() {
-//        inputDistance.value = 'distanza';
         console.log(inputDistance.value);
 
     }
 )
-
+console.log(ticketPrice);
 console.log(inputDistance.value);
 /* console.log(ageRange.value); */
+console.log(messaggio);
