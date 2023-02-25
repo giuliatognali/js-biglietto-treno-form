3b2a72e345dc6621642d9236e13bb98f3c470d41
giuliatognali <giuliatognali@gmail.com>
2023-02-25 14:00:11 +0100

    value e ticketprice standard

--- js/main.js
+++ js/main.js
@@ -1,37 +1,29 @@
 'use strict';
 
+//definizione variabili presneti nel ticket form 
+const inputName = document.getElementById('input-name');
 const inputDistance = document.getElementById('input-distance');
 const ageRange = document.getElementById('age-range');
-
-
-let ticketPrice = inputDistance * 0.21;
-
-let messaggio = 'Prezzo biglietto';
-
-const ageOver18 = document.getElementById('over18');
-const ageUnder18 = document.getElementById('under18');
-const ageOver65 = document.getElementById('over65');
-
 const submitBtn = document.getElementById('submit-btn');
-
-if (ageRange === ageUnder18) {
-    ticketPrice = ticketPrice - ticketPrice * 0.2;
-    messaggio = 'Prezzo con sconto minorenni'
-//    ticketPrice -= ticketPrice * 0,2; altro modo per scrivere quest'operazione
-} 
-else if (ageRange === ageOver65) {
-    ticketPrice = ticketPrice - ticketPrice * 0.4;
-    messaggio = 'Prezzo con sconto over 65'
-}
+const resetBtn = document.getElementById('reset-btn');
 
 
 submitBtn.addEventListener('click', 
     function() {
-        console.log(inputDistance.value);
 
-    }
-)
-console.log(ticketPrice);
+    //valore input
+    const inputNameValue = inputName.value;
+    const inputDistanceValue = Number(inputDistance.value);
+    const ageRangeValue = ageRange.value;
+
+    //prezzo del biglietto
+    let ticketPrice = 0.21 * inputDistanceValue;
+
+    console.log(inputNameValue, inputDistanceValue,ageRangeValue);   
+    console.log(ticketPrice);
+}
+) 
+console.log(inputName.value);
 console.log(inputDistance.value);
-/* console.log(ageRange.value); */
-console.log(messaggio);
+//console.log(ageRange.value);
+/* console.log(messaggio); */
