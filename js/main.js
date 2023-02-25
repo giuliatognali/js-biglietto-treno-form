223004b32bde13daf50ebfb4c6f057d3c89e472c
giuliatognali <giuliatognali@gmail.com>
2023-02-25 14:08:25 +0100

    if ticketprice

--- js/main.js
+++ js/main.js
@@ -8,22 +8,27 @@ const submitBtn = document.getElementById('submit-btn');
 const resetBtn = document.getElementById('reset-btn');
 
 
-submitBtn.addEventListener('click', 
-    function() {
+submitBtn.addEventListener('click',
+    function () {
 
-    //valore input
-    const inputNameValue = inputName.value;
-    const inputDistanceValue = Number(inputDistance.value);
-    const ageRangeValue = ageRange.value;
+        //valore input
+        const inputNameValue = inputName.value;
+        const inputDistanceValue = Number(inputDistance.value);
+        const ageRangeValue = ageRange.value;
 
-    //prezzo del biglietto
-    let ticketPrice = 0.21 * inputDistanceValue;
+        //prezzo del biglietto
+        let ticketPrice = 0.21 * inputDistanceValue;
 
-    console.log(inputNameValue, inputDistanceValue,ageRangeValue);   
-    console.log(ticketPrice);
-}
-) 
-console.log(inputName.value);
-console.log(inputDistance.value);
-//console.log(ageRange.value);
-/* console.log(messaggio); */
+        //condizioni sconto biglietto
+        if (ageRangeValue === 'under18') {
+            ticketPrice = ticketPrice - ticketPrice * 0.2;
+        }
+        else if (ageRangeValue === 'over65') {
+            ticketPrice = ticketPrice - ticketPrice * 0.4;
+        }
+
+        console.log(inputNameValue, inputDistanceValue, ageRangeValue);
+        console.log(ticketPrice);
+    }
+)
+
